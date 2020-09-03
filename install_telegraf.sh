3371438dc22cc89e1f142bb3bbdf3f650d995d9c
ike091 <ike091@gmail.com>
2020-09-03 15:52:43 -0600

    Update telegraf installation file

--- install_telegraf.sh
+++ install_telegraf.sh
@@ -5,3 +5,6 @@
 wget https://dl.influxdata.com/telegraf/releases/telegraf_1.15.2-1_amd64.deb
 
 sudo dpkg -i telegraf_1.15.2-1_amd64.deb
+
+# stop the service, as it automatically starts
+sudo service telegraf stop
