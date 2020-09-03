3d89f8834a38cce77d1c4ad7d556611fb802f03b
ike091 <ike091@gmail.com>
2020-09-03 15:05:30 -0600

    Update snmp manager install script to copy a config file

--- install_snmp_manager.sh
+++ install_snmp_manager.sh
@@ -8,8 +8,5 @@ sudo apt update
 # install snmp and tool for managing mib data
 sudo apt install snmp snmp-mibs-downloader -y
 
-# install snmp daemon
-# sudo apt install snmpd -y
-
-# copy configuration file
-# sudo cp /local/repository/snmpd.conf /etc/snmp/snmpd.conf
+# update configuration file
+sudo cp /local/repository/snmp.conf /etc/snmp/snmp.conf
