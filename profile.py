a9e58518e01714b2c8c5faec069608d023e92c69
ike091 <ike091@gmail.com>
2020-09-15 16:18:56 -0600

    Update kubernetes yaml file

--- profile.py
+++ profile.py
@@ -72,6 +72,7 @@ for i in range(params.node_count):
     # install management software on first node
     if i == 0:
         run_install_script(node, 'install_snmp_manager.sh')
+        run_install_script(node, 'install_minikube.sh')
 
     # run install scripts on each node
     run_install_script(node, 'install_snmp_agent.sh')
