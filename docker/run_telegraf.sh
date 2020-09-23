d6ad51cb5f564d7ceff2e943a450e3d1fa01d53f
ike091 <ike091@gmail.com>
2020-09-23 11:53:11 -0600

    Update run_telegraf script in Docker container to use k8s/helm telegraf.conf

--- docker/run_telegraf.sh
+++ docker/run_telegraf.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# starts the telegraf service
-telegraf --config /usr/src/app/telegraf.conf
+# starts the telegraf service with the baked in config file
+# telegraf --config /usr/src/app/telegraf.conf
+
+# starts the telegraf service wtih the config file from k8s/helm
+telegraf --config /etc/config/telegraf.conf
 
 # run until container shuts down
 while true; do sleep 1000; done
