3047a92cdb576030ab3d999a3ce37e15f19d24cd
ike091 <ike091@gmail.com>
2020-09-25 13:05:13 -0600

    Add -y flag to install_docker.sh

--- install/install_docker.sh
+++ install/install_docker.sh
@@ -8,7 +8,7 @@ sudo apt-get update
 # uninstall possible old versions
 sudo apt-get remove docker docker-engine docker.io containerd runc -y
 
-sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
 
 # add GPG key
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
