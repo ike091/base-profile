5e55cfcaaa1b824969695b321aabc067b7cd01bd
ike091 <ike091@gmail.com>
2020-10-07 11:39:04 -0600

    Update profile to properly allocate extra public IP addresses

--- profile.py
+++ profile.py
@@ -13,7 +13,7 @@ import geni.rspec.igext as igext
 UBUNTU18_IMG = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD'
 CENTOS7_IMG = 'urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD'
 SUPPORTED_HARDWARE_TYPES = ['pc3000', 'd430', 'd710']
-SUPPORTED_OPERATING_SYSTEMS = ['ubuntu', 'centos7']
+SUPPORTED_MODES = ['slate_cluster', 'experiment']
 
 # the number of extra public ip addresses to allocate
 NUM_IP_ADDRESSES = 3
@@ -28,7 +28,7 @@ request = pc.makeRequestRSpec()
 pc.defineParameter('node_count', 'Number of nodes', portal.ParameterType.INTEGER, 2)
 pc.defineParameter('node_type_master', 'Type of physical node to instantiate master controller on', portal.ParameterType.STRING, 'd430')
 pc.defineParameter('node_type_worker', 'Type of physical node to instantiate workers on (pc3000, d430, d710)', portal.ParameterType.STRING, 'd710')
-pc.defineParameter('operating_system', 'Operating system to install on host. (ubuntu or centos7) Note that automatic software install is only supported for Ubuntu at this time.', portal.ParameterType.STRING, 'ubuntu')
+pc.defineParameter('mode', 'Type of experiment to instantiate (slate_cluster or experiment)', portal.ParameterType.STRING, 'slate_cluster')
 
 params = pc.bindParameters()
 
@@ -42,8 +42,8 @@ if params.node_type_worker not in SUPPORTED_HARDWARE_TYPES:
 if params.node_type_master not in SUPPORTED_HARDWARE_TYPES:
     pc.reportError(portal.ParameterError('You must choose a valid hardware type.', ['node_type_master']))
 
-# validate operating system
-if params.operating_system not in SUPPORTED_OPERATING_SYSTEMS:
+# validate mode
+if params.mode not in SUPPORTED_MODES:
     pc.reportError(portal.ParameterError('You must choose a valid operating system.', ['operating_system']))
 
 pc.verifyParameters()
@@ -56,7 +56,7 @@ if params.node_count > 1:
 
 
 def run_install_script(this_node, script_name):
-    """Runs a bash script from the install/ on a specific node."""
+    """Runs a bash script from the install/ directory on a specific node."""
 
     this_node.addService(pg.Execute(shell='sh', command='chmod +x /local/repository/install/' + script_name))
     this_node.addService(pg.Execute(shell='sh', command='/local/repository/install/' + script_name))
@@ -70,21 +70,22 @@ for i in range(params.node_count):
         iface = node.addInterface('eth1')
         lan.addInterface(iface)
 
-    # set the hardware type of each node
-    if i == 0:
-        node.hardware_type = params.node_type_master
-    else:
-        node.hardware_type = params.node_type_worker
+    # set the hardware type of each node in experiment mode
+    if params.mode == 'experiment':
+        if i == 0:
+            node.hardware_type = params.node_type_master
+        else:
+            node.hardware_type = params.node_type_worker
 
 
     # set the OS on each node
-    if params.operating_system == 'ubuntu':
+    if params.mode == 'experiment':
         node.disk_image = UBUNTU18_IMG
-    elif params.operating_system == 'centos7':
+    elif params.mode == 'slate_cluster':
         node.disk_image = CENTOS7_IMG
 
-    # run different scripts based on operating system
-    if params.operating_system == 'ubuntu':
+    # run different scripts based on mode
+    if params.mode == 'experiment':
         # install management software on first node
         if i == 0:
             run_install_script(node, 'install_snmp_manager.sh')
@@ -96,17 +97,17 @@ for i in range(params.node_count):
         run_install_script(node, 'install_snmp_agent.sh')
         run_install_script(node, 'install_docker.sh')
 
-    elif params.operating_system == 'centos7':
-        # put centos7-specific install scripts and configuration here
-        if i == 0:
-            run_install_script(node, 'install_kubernetes_cluster.sh')
-        else:
-            run_install_script(node, 'install_kubernetes_worker_node.sh')
+    #  elif params.mode == 'slate_cluster':
+        # put slate cluster specific install scripts and configuration here (CentOS7)
+        #  if i == 0:
+            #  run_install_script(node, 'install_kubernetes_cluster.sh')
+        #  else:
+            #  run_install_script(node, 'install_kubernetes_worker_node.sh')
 
 
 # request a pool of dynamic publically routable ip addresses
 address_pool = igext.AddressPool('address_pool', NUM_IP_ADDRESSES)
-address_pool.component_manager_id = ('urn:publicid:IDN+utah.emulab.us+authority+cm')
+address_pool.component_manager_id = ('urn:publicid:IDN+utah.cloudlab.us+authority+cm')
 request.addResource(address_pool)
 
 # output RSpec
